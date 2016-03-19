e2c683a8fcf5048945e12dbe5013477308f9373d
wja123 <w.j.alag@gmail.com>
2016-03-19 13:58:33 -0700

    database created

---
+++ travel.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 5.7.11, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: travel
+-- ------------------------------------------------------
+-- Server version	5.7.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `destination`
+--
+
+DROP TABLE IF EXISTS `destination`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `destination` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `image` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `destination`
+--
+
+LOCK TABLES `destination` WRITE;
+/*!40000 ALTER TABLE `destination` DISABLE KEYS */;
+INSERT INTO `destination` VALUES (1,'Toronto','Canada','https://www.porternovelli.com/wp-content/uploads/2014/07/toronto-skyline-night-view.jpg'),(2,'Montreal','Canada','http://www.justapack.com/wp-content/uploads/2015/03/Montreal_night_view.jpg');
+/*!40000 ALTER TABLE `destination` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `itinerary`
+--
+
+DROP TABLE IF EXISTS `itinerary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `itinerary` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `location` varchar(200) DEFAULT NULL,
+  `arrival_date` date DEFAULT NULL,
+  `arrival_time` time DEFAULT NULL,
+  `departure_date` date DEFAULT NULL,
+  `departure_time` time DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `dest_id` mediumint(9) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itinerary`
+--
+
+LOCK TABLES `itinerary` WRITE;
+/*!40000 ALTER TABLE `itinerary` DISABLE KEYS */;
+INSERT INTO `itinerary` VALUES (1,'Holy Chuck','2016-03-19','13:00:00','2016-03-19','14:00:00','Eat a burger',1),(2,'Eaton Center','2016-03-19','15:00:00','2016-03-19','16:00:00',NULL,1);
+/*!40000 ALTER TABLE `itinerary` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-19 13:53:45
